968d58a4d4917b0feeac88f82087b232bcada103
Diego Lopes <dihgg@dihgg.com>
2016-11-06 21:06:38 -0200

    alterações

---
+++ tests/APS-fps.ino
@@ -0,0 +1,8 @@
+#include <FPS_GT511C1R.h>
+#include "src/Fingerprint.h";
+FPS_GT511C1R fps(8, 9);
+Fingerprint fingerprint(&fps);
+
+void setup() {}
+void loop() {}
+
--- tests/APS-lcd.ino
+++ tests/APS-lcd.ino
@@ -1,21 +1,14 @@
 #include <LiquidCrystal.h>
-#include <FPS_GT511C1R.h>
 #include "src/LCDController.h";
-#include "src/Fingerprint.h";
-
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
-FPS_GT511C1R fps(8, 9);
-
 LCDController clcd(&lcd);
-Fingerprint fingerprint(&fps, clcd);
 void setup(){
-  // TESTE DE LCD
+	// TESTE DE LCD
   clcd.setup(); // chamar setup
 }
 void loop(){
     clcd.print("looser", clcd.setCursor(0,1)); //deve escrever na segunda linha
-  delay(2000);
-  clcd.print("looser",clcd.setCursor(0,1), true); // deve escrever apenas na segunda linha
-  delay(2000);
+	delay(2000);
+	clcd.print("looser",clcd.setCursor(0,1), true); // deve escrever apenas na segunda linha
+	delay(2000);
 }
-
