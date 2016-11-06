f655c107766c6f336aa20ed7d01c17a72d39d387
Diego Lopes <dihgg@dihgg.com>
2016-11-06 15:06:29 -0200

    classes iniciais

--- src/Fingerprint.h
+++ src/Fingerprint.h
@@ -1,3 +1,28 @@
+// #include <FPS_GT511C1R.h>
 class Fingerprint {
-	
+	// VARIAVEIS
+	FPS_GT511C1R* fps;
+	public: Fingerprint(FPS_GT511C1R* fps) {
+		this->fps = fps;
+	}
+
+	// METODOS
+	public:
+		setup() {
+			fps->Open();
+		}
+		SetLED(bool on = false) {
+			fps->SetLED(on);
+		}
+		Register() {
+			int id = this->getCount();
+			if(id < 20) {
+				fps->EnrollStart(id);				
+			}
+		}
+
+		// GETTERS AND SETTERS
+		getCount() {
+			return fps->GetEnrollCount();
+		}
 };
